0cd62b9e2ddc8f908f8660449d3d985207e88dbb
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2024-05-13 22:26:08 +0200

    moved additional services to correct directory

--- src/main/java/pl/wsb/hotel/services/LuggageService.java
+++ src/main/java/pl/wsb/hotel/services/LuggageService.java
@@ -1,4 +1,22 @@
 package pl.wsb.hotel.services;
 
-public class LuggageService extends SpecialService{
+
+import pl.wsb.hotel.services.SpecialService;
+
+public class LuggageService extends SpecialService {
+    public LuggageService(String name) {
+        super(name);
+    }
+
+    public void orderService() {
+        System.out.println("przechowywany bagaz klienta: " + getName());
+    }
+
+    public int calculateCost(int quantity, double unitPrice) {
+        return quantity * (int) unitPrice;
+    }
+
+    public boolean highDemand() {
+        return false;
+    }
 }
