7a8e571026d9c5a949d1a6c6f239c2ef2bf88f5f
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2024-05-15 19:31:35 +0200

    Modified some exceptions and interface to default

--- src/main/java/pl/wsb/hotel/interfaces/HotelCapability.java
+++ src/main/java/pl/wsb/hotel/interfaces/HotelCapability.java
@@ -10,25 +10,28 @@ import java.util.Collection;
 
 public interface HotelCapability {
 
-//    // clients //
-        String addClient(String firstName, String lastName, LocalDate birthDate);
-        String getClientFullName(String clientId) throws ClientNotFoundException; //dodane throws KONSULTACJE
-        int getNumberOfUnderageClients();
-//
-//    // rooms //
-        String addRoom(double area, int floor, boolean hasKingSizeBed, String description);
-        double getRoomArea(String roomId) throws RoomNotFoundException; //dodane throws
-        int getNumberOfRoomsWithKingSizeBed(int floor);
-//
-//    // reservations //
-    String addNewReservation(String clientId, String roomId, LocalDate date) throws ClientNotFoundException, RoomNotFoundException, RoomReservedException; //dodane throws
-//
-    String confirmReservation(String reservationId) throws ReservationNotFoundException;
-//
-    boolean isRoomReserved(String roomId, LocalDate date) throws RoomNotFoundException;
-//
-    int getNumberOfUnconfirmedReservation(LocalDate date);
-//
-   Collection<String> getRoomIdsReservedByClient(String clientId) throws ClientNotFoundException; //dodane throws
-}
+    // clients //
+    String addClient(String firstName, String lastName, LocalDate birthDate);
+    String getClientFullName(String clientId);
+    int getNumberOfUnderageClients();
 
+    // rooms //
+    String addRoom(double area, int floor, boolean hasKingSizeBed, String description);
+    double getRoomArea(String roomId);
+    int getNumberOfRoomsWithKingSizeBed(int floor);
+
+    // reservations //
+    String addNewReservation(String clientId, String roomId, LocalDate date)
+            throws ClientNotFoundException, RoomNotFoundException, RoomReservedException;
+
+    String confirmReservation(String reservationId)
+            throws ReservationNotFoundException;
+
+    boolean isRoomReserved(String roomId, LocalDate date)
+            throws RoomNotFoundException;
+
+    int getNumberOfUnconfirmedReservation(LocalDate date);
+
+    Collection<String> getRoomIdsReservedByClient(String clientId)
+            throws ClientNotFoundException;
+}
